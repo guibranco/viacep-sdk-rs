69c75d3b133ad20ac8959a7ce84d302195153354
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2026-03-06 18:11:35 +0000

    Refactor search_address.rs for improved readability

--- src/bin/search_address.rs
+++ src/bin/search_address.rs
@@ -1,28 +1,35 @@
-  
-extern crate dotenv;
-extern crate viacep_rs;
-#[macro_use]
-extern crate prettytable;
-
 use dotenv::dotenv;
+use prettytable::{format, row, Table};
 use viacep_rs::ViaCepClient;
-use prettytable::Table;
-use prettytable::format;
 
-fn main(){
+fn main() {
     dotenv().ok();
 
     let client = ViaCepClient::new();
 
-    match client.search("sp", "são paulo", "paulista"){
+    match client.search("sp", "são paulo", "paulista") {
         Err(e) => eprintln!("{:?}", e),
-        Ok(data) =>{
+        Ok(data) => {
             let addresses = data.unwrap();
             let mut table = Table::new();
             table.set_format(*format::consts::FORMAT_NO_LINESEP_WITH_TITLE);
-            table.set_titles(row!["State", "City", "Neighborhood", "Address", "Complement", "Zip"]);
+            table.set_titles(row![
+                "State",
+                "City",
+                "Neighborhood",
+                "Address",
+                "Complement",
+                "Zip"
+            ]);
             for address in addresses {
-                table.add_row(row![address.state_initials, address.city, address.neighborhood, address.address, address.complement, address.zip]);
+                table.add_row(row![
+                    address.state_initials,
+                    address.city,
+                    address.neighborhood,
+                    address.address,
+                    address.complement,
+                    address.zip
+                ]);
             }
             table.printstd();
         }
